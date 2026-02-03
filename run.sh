8e573aa77c6b7fcd5547ab103579aa89f26af3e9
Simon Beylat <sbeylat@menkar048.lsce.ipsl.fr>
2026-02-03 11:38:41 +0100

    update run.sh

--- run.sh
+++ run.sh
@@ -1,13 +1,25 @@
 #!/usr/bin/env bash
 set -e
+FILE="$1"
 
-git clone https://github.com/BayesExeter/ExeterUQ_MOGP.git
-git clone https://github.com/alan-turing-institute/mogp-emulator.git
-cd mogp-emulator
-git checkout tags/v0.5.0
+if [ -d "ExeterUQ_MOGP" ]; then
+   :
+else
+   git clone https://github.com/BayesExeter/ExeterUQ_MOGP.git
+fi
 
-sed -i 's/^from \.version/#from \.version/' mogp_emulator/__init__.py
 
-cd ../
+
+if [ -d "mogp-emulator" ]; then
+   :
+else
+   git clone https://github.com/alan-turing-institute/mogp-emulator.git
+   cd mogp-emulator
+   git checkout tags/v0.5.0
+   sed -i 's/^from \.version/#from \.version/' mogp_emulator/__init__.py
+   cd ../
+fi
+
+
 docker build -t hm-orchidee .
-docker run --rm -it -v "$PWD/Data:/data" hm-orchidee /data/Raoult2024/RMSD/
+docker run --rm -it -v "$PWD/Data:/data" hm-orchidee /data/$FILE
